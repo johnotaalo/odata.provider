158567f5b364342bd592c5e2f6b1f167d94c2277
Cristian Romanescu <cristian@romanescu.ro>
2013-11-21 14:33:10 +0200

    refs #333 - Moved the version check inside Java function

--- src/org/informea/odata/util/ToolkitUtil.java
+++ src/org/informea/odata/util/ToolkitUtil.java
@@ -617,4 +617,49 @@ public class ToolkitUtil {
                 Producer.MAJOR, Producer.MINOR, Producer.REVISION, 
                 (Producer.BETA ? " beta" : ""));
     }
+
+    /**
+     * Check for toolkit update
+     * @return Structure containing update information
+     */
+    public static Map<String, Object> checkUpdate() {
+        Map<String, Object> ret = new HashMap<String, Object>();
+        boolean needsUpdate = false;
+        ret.put("needsUpdate", needsUpdate);
+        ret.put("success", false);
+        try {
+            URL url = new URL(Producer.UPDATE_URL);
+            URLConnection c = url.openConnection();
+            c.setConnectTimeout(4000);
+            java.io.InputStream fis = c.getInputStream();
+            java.util.Properties props = new java.util.Properties();
+            props.load(fis);
+    
+            int major = Integer.parseInt(props.getProperty("MAJOR"));
+            int minor = Integer.parseInt(props.getProperty("MINOR"));
+            int revision = Integer.parseInt(props.getProperty("REVISION"));
+            boolean beta = Boolean.parseBoolean(props.getProperty("BETA"));
+
+            if(major > Producer.MAJOR ) {
+                needsUpdate = true;
+            } else {
+                if(minor > Producer.MINOR) {
+                    needsUpdate = true;
+                } else {
+                    if(revision > Producer.REVISION) {
+                        needsUpdate = true;
+                    } else {
+                        if(Producer.BETA && !beta) {
+                            needsUpdate = true;
+                        }
+                    }
+                }
+            }
+            ret.put("needsUpdate", needsUpdate);
+            ret.put("success", true);
+        } catch(Exception ex) {
+            log.log(Level.WARNING, "Cannot check for new version!", ex);
+        }
+        return ret;
+    }
 }
