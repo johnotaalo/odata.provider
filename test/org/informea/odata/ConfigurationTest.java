1365e84c0b878f9ee8f87ed33ab9b3601f283ada
Cristian Romanescu <cristian@romanescu.ro>
2013-11-24 21:35:42 +0200

    Commited unit tests for the Configuration class

---
+++ test/org/informea/odata/ConfigurationTest.java
@@ -0,0 +1,45 @@
+package org.informea.odata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfigurationTest {
+
+    @Test
+    public void testGetInstance() {
+        Configuration cfg = Configuration.getInstance();
+        assertNotNull(cfg);
+    }
+
+    @Test
+    public void testGetString() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetString() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetBoolean() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetBoolean() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetInt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetInt() {
+        fail("Not yet implemented");
+    }
+
+}
