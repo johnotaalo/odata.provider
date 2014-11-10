61caf740a93e0190e7fb6613bcae6482bdb3787a
Cristian Romanescu <cristian@romanescu.ro>
2014-11-10 14:58:59 +0200

    Fixed bug where service exposed dates had the wrong time (timezone)

--- src/main/java/edw/olingo/service/InformeaODataJPAServiceFactory.java
+++ src/main/java/edw/olingo/service/InformeaODataJPAServiceFactory.java
@@ -1,5 +1,7 @@
 package edw.olingo.service;
 
+import java.util.TimeZone;
+
 import org.apache.olingo.odata2.api.ODataCallback;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAContext;
 import org.apache.olingo.odata2.jpa.processor.api.exception.ODataJPARuntimeException;
@@ -12,28 +14,26 @@ public class InformeaODataJPAServiceFactory extends org.apache.olingo.odata2.jpa
 
 	@Override
 	public ODataJPAContext initializeODataJPAContext() throws ODataJPARuntimeException {
+		// Assume that all dates received in InforMEA are given in UTC (OData default?).
+		TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC"));
 		ODataJPAContext oDataJPAContext = getODataJPAContext();
 		oDataJPAContext.setEntityManagerFactory(JPAEntityManagerFactory.getEntityManagerFactory(PUNIT_NAME));
 		oDataJPAContext.setPersistenceUnitName(PUNIT_NAME);
 //		oDataJPAContext.setPageSize(PAGE_SIZE);
-//        oDataJPAContext.setDefaultNaming(true);
+//      oDataJPAContext.setDefaultNaming(true);
 		setDetailErrors(true);
         oDataJPAContext.setJPAEdmExtension(new InformeaJPAExtension());
-
         oDataJPAContext.setJPAEdmMappingModel("jpa_edm_mapping.xml");
 		return oDataJPAContext;
 	}
 
     public <T extends ODataCallback> T getCallback(final Class<? extends ODataCallback> callbackInterface) {
         T callback;
-
         if (callbackInterface.isAssignableFrom(InformeaDebugCallback.class)) {
             callback = (T) new InformeaDebugCallback();
         } else {
             callback = (T) super.getCallback(callbackInterface);
         }
-
         return callback;
     }
-
 }
