62de2a7faf9620ab29410e073d9f8d56135b6dfe
Cristian Romanescu <cristian@romanescu.ro>
2014-11-11 20:20:45 +0200

    Added shortcut functions to ServiceInformation for existing entities

--- src/main/java/edw/olingo/service/ServiceInformation.java
+++ src/main/java/edw/olingo/service/ServiceInformation.java
@@ -9,6 +9,13 @@ import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
+import edw.olingo.model.Contact;
+import edw.olingo.model.CountryReport;
+import edw.olingo.model.Decision;
+import edw.olingo.model.Meeting;
+import edw.olingo.model.NationalPlan;
+import edw.olingo.model.Site;
+
 public class ServiceInformation {
 
 	public static final int VERSION_MAJOR_VERSION = 2;
@@ -32,5 +39,29 @@ public class ServiceInformation {
 			log.log(Level.WARNING, "Error getting entity count for %s", ex);
 		}
 		return count;
-	}	
+	}
+
+	public static long countMeetings() {
+		return ServiceInformation.countEntities(Meeting.class);
+	}
+
+	public static long countContacts() {
+		return ServiceInformation.countEntities(Contact.class);
+	}
+
+	public static long countDecisions() {
+		return ServiceInformation.countEntities(Decision.class);
+	}
+
+	public static long countCountryReports() {
+		return ServiceInformation.countEntities(CountryReport.class);
+	}
+
+	public static long countNationalPlans() {
+		return ServiceInformation.countEntities(NationalPlan.class);
+	}
+
+	public static long countSites() {
+		return ServiceInformation.countEntities(Site.class);
+	}
 }
