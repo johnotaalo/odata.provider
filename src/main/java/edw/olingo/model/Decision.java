17be7d93485366c72181845767944772a6c7b67b
Cristian Romanescu <cristian@romanescu.ro>
2014-11-12 20:08:48 +0200

    Added proper getters to Decision entity

--- src/main/java/edw/olingo/model/Decision.java
+++ src/main/java/edw/olingo/model/Decision.java
@@ -18,114 +18,136 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
- * Decision primary entity
- * <br />
+ * Decision primary entity <br />
+ * 
  * @author Cristian Romanescu {@code cristian.romanescu _at_ eaudeweb.ro}
- * @version 1.4.0, 10/28/2011
+ * @version 2.0.0, 11/12/2014
  * @since 1.3.3
  */
 @Entity
 @Table(name = "informea_decisions")
 public class Decision {
 
-    private static final Logger log = Logger.getLogger(Decision.class.getName());
-    @Id
-    @Column(name = "id")
-    private String id;
-    private String link;
-    @Column (nullable = false)
-    private String number;
-    @Column (nullable = false)
-    private String status;
-    @Column (nullable = false)
-    private String type;
-    @Column (nullable = false)
-    private String treaty;
-    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
-    private Date published;
-    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
-    private Date updated;
-    @Column(name="meetingId")
-    private String meetingId;
-    @Column(name="meetingTitle")
-    private String meetingTitle;
-    @Column(name="meetingUrl")
-    private String meetingUrl;
+	@Id
+	@Column(name = "id")
+	private String id;
+	private String link;
+	@Column(nullable = false)
+	private String number;
+	@Column(nullable = false)
+	private String status;
+	@Column(nullable = false)
+	private String type;
+	@Column(nullable = false)
+	private String treaty;
+	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
+	private Date published;
+	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
+	private Date updated;
+	@Column(name = "meetingId")
+	private String meetingId;
+	@Column(name = "meetingTitle")
+	private String meetingTitle;
+	@Column(name = "meetingUrl")
+	private String meetingUrl;
 
-    @OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
-    private List<DecisionTitle> titles = new ArrayList<DecisionTitle>();
+	@OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
+	private List<DecisionTitle> titles = new ArrayList<DecisionTitle>();
 
-    @OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
-    private List<DecisionLongTitle> longTitles = new ArrayList<DecisionLongTitle>();
+	@OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
+	private List<DecisionLongTitle> longTitles = new ArrayList<DecisionLongTitle>();
 
-    @OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
-    private List<DecisionSummary> summaries = new ArrayList<DecisionSummary>();
+	@OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
+	private List<DecisionSummary> summaries = new ArrayList<DecisionSummary>();
 
-    @OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
-    private List<DecisionContent> contents = new ArrayList<DecisionContent>();
+	@OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
+	private List<DecisionContent> contents = new ArrayList<DecisionContent>();
 
-    @OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
-    private List<DecisionKeyword> keywords = new ArrayList<DecisionKeyword>();
+	@OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
+	private List<DecisionKeyword> keywords = new ArrayList<DecisionKeyword>();
 
-    @OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
-    private List<DecisionFile> files = new ArrayList<DecisionFile>();
+	@OneToMany(mappedBy = "decision", cascade = CascadeType.ALL)
+	private List<DecisionFile> files = new ArrayList<DecisionFile>();
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getLink() {
-        return link;
-    }
+	public String getLink() {
+		return link;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public String getNumber() {
-        return this.number;
-    }
+	public String getNumber() {
+		return this.number;
+	}
 
-    public String getTreaty() {
-        return treaty;
-    }
+	public String getTreaty() {
+		return treaty;
+	}
 
-    public Date getPublished() {
-        return this.published;
-    }
+	public Date getPublished() {
+		return this.published;
+	}
 
-    public Date getUpdated() {
-        return updated;
-    }
+	public Date getUpdated() {
+		return updated;
+	}
 
-    public String getMeetingId() {
-        return meetingId;
-    }
+	public String getMeetingId() {
+		return meetingId;
+	}
 
-    public void setMeetingId(String meetingId) {
-        this.meetingId = meetingId;
-    }
+	public void setMeetingId(String meetingId) {
+		this.meetingId = meetingId;
+	}
 
-    public String getMeetingTitle() {
-        return meetingTitle;
-    }
+	public String getMeetingTitle() {
+		return meetingTitle;
+	}
 
-    public void setMeetingTitle(String meetingTitle) {
-        this.meetingTitle = meetingTitle;
-    }
+	public void setMeetingTitle(String meetingTitle) {
+		this.meetingTitle = meetingTitle;
+	}
 
-    public String getMeetingUrl() {
-        return meetingUrl;
-    }
+	public String getMeetingUrl() {
+		return meetingUrl;
+	}
 
-    public void setMeetingUrl(String meetingUrl) {
-        this.meetingUrl = meetingUrl;
-    }
+	public void setMeetingUrl(String meetingUrl) {
+		this.meetingUrl = meetingUrl;
+	}
+
+	public List<DecisionTitle> getTitles() {
+		return titles;
+	}
+
+	public List<DecisionLongTitle> getLongTitles() {
+		return longTitles;
+	}
+
+	public List<DecisionSummary> getSummaries() {
+		return summaries;
+	}
+
+	public List<DecisionContent> getContents() {
+		return contents;
+	}
+
+	public List<DecisionKeyword> getKeywords() {
+		return keywords;
+	}
+
+	public List<DecisionFile> getFiles() {
+		return files;
+	}
 }
