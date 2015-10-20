a2a6f1877d48f66b604c801dd90d7b4dca6096ef
Cristian Romanescu <cristian@romanescu.ro>
2015-10-20 12:51:27 +0300

    Added script to populate the test database

--- tests-prepare.sh
+++ tests-prepare.sh
@@ -3,6 +3,6 @@
 # To run tests, you have to populate the 'informea_drupal' database with real data
 # This script will re-create the views for the toolkit inside informea_odata_test database
 
-mysql -e "DROP DATABASE IF EXISTS informea_odata_test; CREATE DATABASE informea_odata_test"
-mysql informea_odata_test < ../../../etc/informea_odata.sql
+mysql -e "DROP DATABASE IF EXISTS informea_odata_test_source; CREATE DATABASE informea_odata_test_source"
+mysql informea_odata_test_source < ./etc/informea_odata.sql
 
