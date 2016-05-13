c8cd0fd57cd50e41d5386238e39abb1b8740a701
Cristian Romanescu <cristian@romanescu.ro>
2016-05-13 14:37:04 +0300

    Added country field to informea_documents

--- etc/cms_document_schema.sql
+++ etc/cms_document_schema.sql
@@ -52,6 +52,7 @@ CREATE OR REPLACE VIEW informea_documents AS
     treaty.treaty,
     REPLACE(thumbnails.uri, 'public://', 'http://www.cms.int/sites/default/files') thumbnailUrl,
     0 displayOrder,
+    UPPER(ciso.field_country_iso3_value) country,
     node.nid
   FROM node node
     LEFT JOIN field_data_field_publication_published_date pdate ON node.nid = pdate.entity_id
@@ -59,10 +60,13 @@ CREATE OR REPLACE VIEW informea_documents AS
     INNER JOIN informea_treaty_machine_name treaty ON treaty.nid = instr.field_instrument_target_id
     LEFT JOIN field_data_field_publication_image img ON node.nid = img.entity_id
     LEFT JOIN file_managed thumbnails ON field_publication_image_fid = thumbnails.fid
+    LEFT JOIN field_data_field_country country ON country.entity_id = node.nid
+    LEFT JOIN field_data_field_country_iso3 ciso ON country.field_country_target_id = ciso.entity_id
   WHERE
     treaty IS NOT NULL
    AND node.type = 'publication'
    GROUP BY node.nid;
+
 
 
 --
